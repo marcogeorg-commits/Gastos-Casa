f12304f9b879e798cd44172a7ebf99a9ddc7ab52
marcogeorg-commits <marcogeorg@yahoo.com>
2026-06-02 15:10:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,32 @@
-const CACHE = 'gastos-casa-v1';
+const CACHE = 'gastos-casa-v2';
 const FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/Gastos-Casa/',
+  '/Gastos-Casa/index.html',
+  '/Gastos-Casa/manifest.json',
+  '/Gastos-Casa/icon-192.png',
+  '/Gastos-Casa/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(c => c.addAll(FILES))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+      .catch(() => caches.match('/Gastos-Casa/index.html'))
   );
 });
